a096cfbdc3d164cd10ef8b38e7340ff71ed17723
cesarbanzan <71900706+cesarbanzan@users.noreply.github.com>
2023-11-21 18:02:58 -0300

    Add files via upload
    se agrega un tiempo para buscar al jugador una vez que se aleja del enemigo

--- Assets/scripts/seguirjugador.cs
+++ Assets/scripts/seguirjugador.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class seguirjugador : MonoBehaviour {
+    public class seguirjugador : MonoBehaviour {
     public Transform jugador;
     public Transform randomPosition;
 
     UnityEngine.AI.NavMeshAgent enemigo;
     public bool dentro = false;
     public bool check;
+    public bool buscar;
+
+    public float tiempoBusqueda = 10f;  
 
 	// Use this for initialization
 	void Start () {
@@ -21,7 +24,7 @@ public class seguirjugador : MonoBehaviour {
     {
          if (other.tag == "Objetivo")
         {
-            check=true;
+        
             enemigo.destination = randomPosition.position;
             Debug.Log(enemigo.destination);
         }
@@ -37,6 +40,7 @@ public class seguirjugador : MonoBehaviour {
     {
         if (other.tag == "Player")
         {
+            StartCoroutine(actualizarCronometro());
             dentro = false;
         }
        
@@ -47,14 +51,36 @@ public class seguirjugador : MonoBehaviour {
 		
         if (dentro)
         {
+            check=true;
+            tiempoBusqueda=10f;
+           
+        }
+        if(check)
+        {
             enemigo.destination = jugador.position;
         }
-        if (!dentro)
+        if (!check)
         {
             enemigo.destination = randomPosition.position;
-          
-        
         }
+            if(tiempoBusqueda< 0f)
+            {
+               
+                check=false;
+                Debug.Log("se escapo el jugador");
+                tiempoBusqueda=10f;
+                  StopAllCoroutines();
+            }
 
 	}
+     IEnumerator actualizarCronometro()
+    {
+        while (true)
+        {
+            Debug.Log("persiguiendo jugador"+tiempoBusqueda);
+            tiempoBusqueda = tiempoBusqueda - Time.deltaTime;
+             yield return null;
+        }
+       
+    }
 }
