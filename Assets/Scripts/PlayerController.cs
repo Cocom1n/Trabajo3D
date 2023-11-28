aa6e9c02cb3aab217f67eb0fa004e26fcc025a15
Barry <barrionuevoluiscarlos51@gmail.com>
2023-11-27 21:59:37 -0300

    commit 5

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -24,7 +24,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         UpdateMove();
         UpdateMouseMove();
@@ -38,23 +38,30 @@ public class PlayerController : MonoBehaviour
 
         if (hor != 0)
         {
-            Debug.Log("horizontal: " + hor);
+<<<<<<< Updated upstream
             transform.Rotate(0, hor * sensitivity.x, 0);
+=======
+            transform.Rotate(0, hor * sensitivity.x, 0);//rota la camara acorde al eje del player
+>>>>>>> Stashed changes
         }
         if (ver != 0)
         {
-            Vector3 rotation = cameraPlayer.localEulerAngles;
-            rotation.x = (rotation.x - ver * sensitivity.y + 360) % 360;
-            if (rotation.x > 80 && rotation.x < 180)
-            {
-                rotation.x = 80;
+            Vector3 rotation = cameraPlayer.localEulerAngles;//obtiene la rotacion de la camara
+            rotation.x = (rotation.x - ver * sensitivity.y + 360) % 360; //actualiza la rotacion en X pero q este debajo del 360
+            if (rotation.x > 40 && rotation.x < 180)
+            {//verifica si la rotacion esta entre 40 y 180 y si llega al limite inferior mantenerlo ahi
+                rotation.x = 40;
             }
             else if (rotation.x < 280 && rotation.x > 180)
-            {
+            {//verifica si la rotacion esta entre 280 y 180 y si llega al limite superior mantenerlo ahi
                 rotation.x = 280;
             }
+<<<<<<< Updated upstream
             Debug.Log("vertical: " + ver);
             cameraPlayer.localEulerAngles = rotation;
+=======
+            cameraPlayer.localEulerAngles = rotation; //aplica la rotacion actualizada
+>>>>>>> Stashed changes
         }
     }
 
@@ -62,8 +69,8 @@ public class PlayerController : MonoBehaviour
     public void UpdateMove()
     {
         // para el despazamiento del player
-        float hor = Input.GetAxisRaw("Horizontal");// en eje (X)
-        float ver = Input.GetAxisRaw("Vertical");// en eje (Z)
+        float hor = Input.GetAxis("Horizontal");// en eje (X)
+        float ver = Input.GetAxis("Vertical");// en eje (Z)
 
         // if (hor != 0.0f || ver != 0.0f)
         // {
@@ -79,14 +86,24 @@ public class PlayerController : MonoBehaviour
         }
         mover.y = rb.velocity.y; // se aplica gravedad
         rb.velocity = mover;
+<<<<<<< Updated upstream
         tocaSuelo = Physics.CheckSphere(groundcheck.position, .2f, ground); //revisa si toca el suelo con el Layer del Plano
         tocaPared = Physics.CheckSphere(groundcheck.position, .6f, walls);
         if(tocaPared == true && tocaSuelo != true){
             mover.y = rb.velocity.y * 2f;
+=======
+        tocaSuelo = Physics.CheckSphere(groundcheck.position, .6f, ground); //revisa si toca el suelo con el Layer del Plano
+        tocaPared = Physics.CheckSphere(groundcheck.position, .6f, walls);// revisa si toca la pared con el layer de las paredes
+
+        if(tocaPared == true && tocaSuelo != true){ //verifica si toca la pared y no el suelo
+            // mover.y = rb.velocity.y * 2f;
+            rb.velocity = new Vector3(0f, -.5f, 0f) * moveSpeed;//cae hasta que toca el suelo
+>>>>>>> Stashed changes
         }
-        if (Input.GetButtonDown("Jump") && tocaSuelo)
+
+        if (Input.GetButtonDown("Jump") && tocaSuelo)//verifica si se aprieta el boton space y si tocasuelo es verdadero
         {
-            rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse); //aplica impulso del salto
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse); //aplica impulso inmediato de salto al personaje
         }
     }
 }
